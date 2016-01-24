9fd77a7bc4b959f47e060c6b62fc52d7315c4afd
michaelreiter <mreiter@uvic.ca>
2016-01-23 22:04:03 -0800

    Add health slider for player

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -6,7 +6,9 @@ public class PlayerHealth : MonoBehaviour {
 
 	public int startingHealth = 100;
 	public int currentHealth;
-	//public Image damageImage;
+	public Slider healthSlider;
+	public Image damageImage;
+	public Color damageColor = new Color(1f,0f,0f,0.1f);
 
 	private Animator anim;
 	bool isDead;
@@ -14,15 +16,15 @@ public class PlayerHealth : MonoBehaviour {
 
 	void Awake() {
 		anim = GetComponent<Animator>();
-
 		currentHealth = startingHealth;
+		healthSlider.value = currentHealth;
 	}
 
 	void Update() {
 		if (damaged) {
-			//
+			damageImage.color = new Color(damageImage.color.r, damageImage.color.g, damageImage.color.b, damageImage.color.a + 0.1f);
 		} else {
-			//
+
 		}
 
 		damaged = false;
@@ -30,8 +32,8 @@ public class PlayerHealth : MonoBehaviour {
 
 	public void TakeDamage(int amount) {
 		damaged = true;
-
 		currentHealth -= amount;
+		healthSlider.value = currentHealth;
 
 		if (currentHealth <= 0 && !isDead) {
 			Death();
