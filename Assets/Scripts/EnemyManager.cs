fd7b3def0fb4a8b568e66aa6c1e3dd931f179907
michaelreiter <mreiter@uvic.ca>
2016-01-26 10:34:04 -0800

    Add menu assets and start building UI

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnInterval;
+	public Transform spawnPoint;
+
+	void Start () {
+		InvokeRepeating("SpawnEnemy", spawnInterval, spawnInterval);
+	}
+
+	void SpawnEnemy() {
+		Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+	}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	
+	public static float score;
+	public int scoreModifier;
+	public Text scoreText;
+
+	void Awake() {
+		scoreText = GetComponent<Text>();
+		score = 0;
+	}
+
+	void Update () {
+		score += Time.deltaTime;
+		scoreText.text = "" + (int)score * scoreModifier;
+
+	}
+}
